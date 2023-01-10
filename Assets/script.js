6aaa52fa21e4d2a41f301830ddef5b24c8e9edb2
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-01-09 20:49:14 -0500

    got my js functioning...need to figure out why 9am is green and if i used the right date format

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,3 @@
-$(document).ready(function() {
-    console.log("Ready!");
-});
-
 let now = moment().format('MMMM Do YYYY, h:mm:ss a');
 let displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = now;
@@ -32,13 +28,14 @@ $(".saveBtn").click(function (event) {
     localStorage.setItem(time,value);
 });
 
-$("#hour-09 .time-block").val(localStorage.getItem("09"));
-$("#hour-10 .time-block").val(localStorage.getItem("10"));
-$("#hour-11 .time-block").val(localStorage.getItem("11"));
-$("#hour-12 .time-block").val(localStorage.getItem("12"));
-$("#hour-13 .time-block").val(localStorage.getItem("13"));
-$("#hour-14 .time-block").val(localStorage.getItem("14"));
-$("#hour-15 .time-block").val(localStorage.getItem("15"));
-$("#hour-16 .time-block").val(localStorage.getItem("16"));
-$("#hour-17 .time-block").val(localStorage.getItem("17"));
+    $("#hour-09 .time-block").val(localStorage.getItem("09"));
+    $("#hour-10 .time-block").val(localStorage.getItem("10"));
+    $("#hour-11 .time-block").val(localStorage.getItem("11"));
+    $("#hour-12 .time-block").val(localStorage.getItem("12"));
+    $("#hour-13 .time-block").val(localStorage.getItem("13"));
+    $("#hour-14 .time-block").val(localStorage.getItem("14"));
+    $("#hour-15 .time-block").val(localStorage.getItem("15"));
+    $("#hour-16 .time-block").val(localStorage.getItem("16"));
+    $("#hour-17 .time-block").val(localStorage.getItem("17"));
+
 
